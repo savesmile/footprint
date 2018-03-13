eb8ec54bde60668109d3fc56d81a60078ac349d5
[F_junlin] <[fengjunlin@23mofang.com]>
2018-03-13 18:03:31 +0800

    fix: 网关服务
    shell脚本

---
+++ gateway/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+mvn clean package -Dmaven.test.skip=true -pl 23mofang-app-gateway -f ../pom.xml -am
+if [ $? -ne 0 ]; then
+    echo "\n [BUILD ERROR]mvn install failed"
+    exit 1
+fi
+
+all=$*
+
+if ${#all[*]} -eq 0; then
+    all[0]='latest'
+fi
+
+for version in ${all}
+do
+    echo ${version}
+    sudo docker build -t gateway:${version} .
+    sudo docker tag gateway:${version} hub.c.163.com/mofang23/gateway:${version}
+    sudo docker push hub.c.163.com/mofang23/gateway:${version}
+done
