ded92b09744953f3a9960915ff09742cc14c13f4
[F_junlin] <[fengjunlin@23mofang.com]>
2018-03-28 17:28:03 +0800

    添加common项目

---
+++ common/src/main/java/com/f_lin/common/CommonApplication.java
@@ -0,0 +1,12 @@
+package com.f_lin.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonApplication.class, args);
+	}
+}
